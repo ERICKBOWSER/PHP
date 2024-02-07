e0af5c648ef4dd43e7fdffced2f71a11e81e673e
Guerig Eri <guerig.eri@gmail.com>
2024-02-07 12:34:55 +0100

    -_-

--- AAAAH PRACTICA FINAL/examen-practicar/Examen_SW_22_23/servicios_rest/src/funciones_servicios.php
+++ AAAAH PRACTICA FINAL/examen-practicar/Examen_SW_22_23/servicios_rest/src/funciones_servicios.php
@@ -33,4 +33,68 @@ function conexion_mysqli()
     }
     return $respuesta;
 }
+
+function login($lector, $clave){
+    try{
+        $conexion = new PDO("mysqli:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQLI_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    }catch(PDOException $e){
+        $respuesta["error"] = "Imposible conectar:" . $e->getMessage();
+    }
+
+    try{
+        $consulta = "SELECT * FROM usuarios WHERE lector = ? AND clave = ?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia -> execute([$lector, $clave]);
+    }catch(PDOException $e){
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["error"] = "Imposible realizar la consulta: " . $e->getMessage();
+    }
+
+    if($sentencia -> rowCount() > 0){
+        // Inicio de sesiones
+        session_name("examen-practicar");
+        session_start();
+        $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+        $respuesta["api_session"] = session_id();
+        $_SESSION["usuario"] = $respuesta["usuario"]["lector"];
+        $_SESSION["clave"] = $respuesta["usuario"]["clave"];
+        $_SESSION["tipo"] = $respuesta["usuario"]["tipo"];
+    }else{
+        $respuesta["mensaje"] = "El usuario no se encuentra en la BD";
+    }
+    $conexion = null;
+    $sentencia = null;
+    return $respuesta;
+}
+
+function logueado($lector, $clave){
+    try{
+        $conexion = new PDO("mysqli:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQLI_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")) ;
+
+    }catch(PDOException $e){
+        $respuesta["error"] = "Imposible conectar:" . $e->getMessage();
+    }
+
+    try{
+        $consulta = "SELECT * FROM usuarios WHERE lector = ? AND clave = ?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$lector, $clave]);
+    }catch(PDOException $e){
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["error"] = "Imposible realizar la consulta:" . $e->getMessage();
+    }
+}
+
+
+
+
+
+
+
+
+
+
 ?>
+
